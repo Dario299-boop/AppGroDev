d8fc3ebdbaea61456d943cebab681e85e31bf1cc
Kenstar05 <kenstar_atomo_05@hotmail.com>
2025-08-18 19:37:42 -0300

    'cambioElTitulo'

--- pages/tareas.php
+++ pages/tareas.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Gestión de Tareas</title>
+  <title>TITULO</title>
   <style>
     * {
       box-sizing: border-box;
